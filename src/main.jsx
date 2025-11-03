b25d978d5062ba5a0788b925322fc8cf126d7c1e
catluvies <anyararosso@gmail.com>
2025-11-03 03:33:35 -0300

    feat: agregar bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { CarritoProvider } from './context/CarritoContext'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './custom.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
